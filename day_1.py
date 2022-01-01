9392a5c34cc8fa52e9122bbf0757c39329af29ba
Amgaa <amgaa.khosbayar@gmail.com>
2022-01-01 07:41:15 -0800

    Python day 1

---
+++ day_1.py
@@ -0,0 +1,26 @@
+# String pring
+#  \n makes new line
+# 
+# print("Hello wolrd!")
+# name = input("What is your name?")
+# print(name)
+
+
+# excercise 
+# a = input("a:")
+# b = input("b:")
+
+# c = b
+# b = a
+# a = c
+
+# print("a: " + a )
+# print("b: " + b )
+
+# Band name generator
+print("Welcome to the Band Name Generator.")
+city = input("What is the city name you grew up in?\n")
+# print(city)
+pet_name = input("What's your pet's name?\n")
+# print(pet_name)
+print("Your band name could be " + city + " " + pet_name)
