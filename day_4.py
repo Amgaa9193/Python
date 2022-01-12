098b1f089cd780992fdbfc28121f23defa0848c5
Amgaa <amgaa.khosbayar@gmail.com>
2022-01-12 07:18:23 -0800

    Started day 4

---
+++ day_4.py
@@ -0,0 +1,29 @@
+import random
+
+# what is module?
+# Modules are responsible for different functionality 
+
+# random_integer = random.randint(1,10)
+# print(random_integer)
+
+# random_float = random.random()
+# print(random_float)
+
+# make the float between 0 - 5
+# num = random_float * 5
+# print(num)
+
+# random_side = random.randint(1,2)
+
+# if random_side == 1:
+#   print("Head")
+# else:
+#   print("Tail")
+
+# List in Python => it is a data structure, way of organizing and organizing data
+
+names_string = input("Give me everybody's name, seperated by a comma.")
+
+names = names_string.split(",")
+
+# Iterate over the names arr
